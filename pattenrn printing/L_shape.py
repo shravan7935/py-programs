844ea9ca8696a2f3157a71299bf449d578fd84aa
shravan <shravan7935@gmail.com>
2024-12-04 18:32:12 +0530

    hhh

---
+++ pattenrn printing/L_shape.py
@@ -0,0 +1,15 @@
+# *
+# *
+# *
+# *
+# *
+# *
+# *******
+
+for row in range (7):
+    for col in range(7):
+        if (col == 0) or (row == 6 and (col !=0)):
+            print("*", end = "")
+        else:
+            print(end = " ")
+    print()
